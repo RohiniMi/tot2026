7d3acb6e1ccad8b4b756d259452c8b33d0dfbcb5
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2026-01-21 12:13:04 +0530

    hide btn added

--- react/fetchFun.js
+++ react/fetchFun.js
@@ -2,23 +2,23 @@ const products = document.getElementById("products");
 const btn = document.getElementById("btn");
 const loader = document.getElementById("loader");
 
-const fun = async () => {
+const showData = (res, table) => {
+    res.map((d) => {
+        const div = document.createElement("div");
+        div.className = "prod";
+        div.innerHTML = d.description;
+        div.style.border = "2px solid red";
+        products.appendChild(div);
+    })
+}
+const getData = async () => {
     try {
-        loader.innerHTML = `<>loding...</>`
+        loader.innerHTML = `<>loading...</>`
         const data = await fetch("https://fakestoreapi.com/products");
         const res = await data.json();
         const table = document.createElement("table");
-        res.map((d) => {
-            const td = document.createElement("td");
-            td.className = "prod";
-            td.innerHTML = d.description;
-            console.log("Hi");
-            td.style.border = "2px solid red";
-            console.log("Hi2");
-            table.appendChild(td);
-            products.appendChild(table);
-        })
-
+        showData(res, table);
+        btn.innerHTML = "click me to hide data";
     } catch (error) {
         console.log(error);
     } finally {
@@ -27,5 +27,10 @@ const fun = async () => {
 }
 btn.addEventListener("click", (e) => {
     e.preventDefault();
-    fun();
+    if (btn.innerHTML === "click me to get data")
+        getData();
+    else {
+        products.innerHTML = "";
+        btn.innerHTML = "click me to get data";
+    }
 })
