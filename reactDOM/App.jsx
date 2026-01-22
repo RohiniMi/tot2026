a2a97fcaaf33671c2747679c74479e327e5e7aa4
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2026-01-22 10:05:23 +0530

    react with babel

--- reactDOM/App.jsx
+++ reactDOM/App.jsx
@@ -1,3 +1,4 @@
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const reactElement = React.createElement("div",{},"Hey")
+// const reactElement = React.createElement("div",{},"Hey")
+const reactElement = <div className="container">Welcome</div>
 root.render(reactElement);
